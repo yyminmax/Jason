1d4deaeb94e7a213a81927d2b849e81c111bf147
yyminmax <yyminmax@gmail.com>
2017-01-19 21:28:13 +0800

    Create quality.h

---
+++ quality.h
@@ -0,0 +1,20 @@
+
+
+
+
+#ifndef QUALITY_H_
+#define QUALITY_H_
+
+#include<string>
+
+using namespace std;
+
+class PreProcess
+{
+    public:
+        void cutadapter(string fq1, string fq2, bool flag);
+        void qualty_trim()
+
+}
+
+#endif
